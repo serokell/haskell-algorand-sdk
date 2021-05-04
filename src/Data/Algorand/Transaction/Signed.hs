serokell/haskell-algorand-sdk

commit 0edbe1840cae1a1935dba0f10fb74587b29926f8

Set sender in all signing functions
Previously, `signSimple` would only add the actual signature. The caller
was responsible for filling in the `tSender` field correctly.

Now this function will overwrite the field with the sender address
corresponding to the signing key. This makes its behaviour consistent
with `signAsContractAccount`.

src/Data/Algorand/Transaction/Signed.hs
@@ -23,8 +23,8 @@ import Data.String (IsString)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
-import Crypto.Algorand.Signature (SecretKey, Signature, sign, verify)
-import Data.Algorand.Address (fromContractCode, toPublicKey)
+import Crypto.Algorand.Signature (SecretKey, Signature, sign, toPublic, verify)
+import Data.Algorand.Address (fromContractCode, fromPublicKey, toPublicKey)
 import Data.Algorand.MessagePack (MessagePackObject (toCanonicalObject), MessageUnpackObject (fromCanonicalObject), (&), (&<>), (.=), (.=<), (.:?), (.:??), (.:>), (.:>?), NonZeroValue (isNonZero))
 import Data.Algorand.Transaction (Transaction (..), serialiseTx)
 import Network.Algorand.Node.Api.Json (defaultOptions)
@@ -51,9 +51,11 @@ data SignedTransaction = SignedTransaction
 {- Simple signature -}
 
 -- | Sign a transaction with a simple signature.
+--
+-- Note: this function will overwrite the sender of the transaction!
 signSimple :: SecretKey -> Transaction -> SignedTransaction
 signSimple sk txn = SignedTransaction
-  { stTxn = txn
+  { stTxn = txn { tSender = fromPublicKey (toPublic sk) }
   , stSig = SignatureSimple $ sign sk (serialiseTx txn)
   }
 
@@ -87,6 +89,8 @@ instance NonZeroValue LogicSignature where
   isNonZero _ = True
 
 -- | Sign a transaction from a contract account.
+--
+-- Note: this function will overwrite the sender of the transaction!
 signFromContractAccount
   :: ByteString  -- ^ Compiled contract code.
   -> [ByteString]  -- ^ Program arguments.

app/halgo/Main.hs
@@ -229,13 +229,12 @@ cmdTxnShowUnsigned json base64 =
   (if json then readItemsJson else readItemsB64 @T.Transaction)
   >>= if base64 then putItemsB64 else putItemsJson
 
--- | Sign transactions.
+-- | Sign transactions with a simple signature.
 cmdTxnSign :: MonadSubcommand m => Bool -> FilePath -> m ()
 cmdTxnSign json skFile = do
   sk <- loadAccount skFile
   txns <- if json then readItemsJson else readItemsB64
-  let txns' = map (\txn -> txn { T.tSender = A.fromPublicKey $ S.toPublic sk }) txns
-  let signed = map (TS.signSimple sk) txns'
+  let signed = map (TS.signSimple sk) txns
   putItemsB64 signed
 
 -- | Transaction ID.

test/algorand-lib/Test/Data/Algorand/Transaction.hs
@@ -15,7 +15,7 @@ import Data.MessagePack (pack, unpack)
 import Data.Proxy (Proxy (Proxy))
 import GHC.TypeLits (KnownNat, natVal)
 
-import Hedgehog (MonadGen, Property, forAll, property, tripping)
+import Hedgehog (MonadGen, Property, (===), forAll, property, tripping)
 import qualified Hedgehog.Gen as G
 import qualified Hedgehog.Range as R
 import Test.Tasty.HUnit (Assertion, (@?=))
@@ -24,7 +24,7 @@ import Crypto.Algorand.Signature (skFromBytes, toPublic)
 import Data.Algorand.Address (Address, fromContractCode, fromPublicKey)
 import Data.Algorand.MessagePack (Canonical (Canonical, unCanonical), EitherError)
 import Data.Algorand.Transaction (StateSchema (..), Transaction (..), TransactionType (..))
-import Data.Algorand.Transaction.Signed (signFromContractAccount, signSimple, verifyTransaction)
+import Data.Algorand.Transaction.Signed (getUnverifiedTransaction, signFromContractAccount, signSimple, verifyTransaction)
 
 import Test.Crypto.Algorand.Signature (genPublicKey, genSecretKeyBytes)
 import Test.Data.Algorand.Program (genProgram)
@@ -106,12 +106,27 @@ hprop_sign_verify_simple = property $ do
   let tx' = tx { tSender = fromPublicKey (toPublic sk) }
   tripping tx' (signSimple sk) verifyTransaction
 
+hprop_signSimple_sets_sender :: Property
+hprop_signSimple_sets_sender = property $ do
+  Just sk <- skFromBytes <$> forAll genSecretKeyBytes
+  tx <- forAll genTransaction
+  let tx' = getUnverifiedTransaction $ signSimple sk tx
+  tSender tx' === fromPublicKey (toPublic sk)
+
+
 hprop_sign_verify_contract :: Property
 hprop_sign_verify_contract = property $ do
   program <- forAll genProgram
   tx <- forAll genTransaction
   let tx' = tx { tSender = fromContractCode program }
   tripping tx' (signFromContractAccount program []) verifyTransaction
+
+hprop_signFromContractAccount_sets_sender :: Property
+hprop_signFromContractAccount_sets_sender = property $ do
+  program <- forAll genProgram
+  tx <- forAll genTransaction
+  let tx' = getUnverifiedTransaction $ signFromContractAccount program [] tx
+  tSender tx' === fromContractCode program
 
 
 unit_simple :: Assertion

commit b5dd8063f83bf8dc2e9e1e8fcc1bf4e564b7ba46

[SDAO-385] Fix simple transaction signature
Commit 0edbe1840cae1a19 introduced a bug where transactions
were signed before setting the sender field.

This restores it to how it was.

To verify, run the following commands:

halgo acc new ./example.acc
halgo acc new ./another.acc
halgo txn new pay $(halgo acc show ./another.acc) 100000 >/tmp/transaction
cat /tmp/transaction | halgo txn sign ./example.acc | halgo txn show

src/Data/Algorand/Transaction/Signed.hs
@@ -54,10 +54,12 @@ data SignedTransaction = SignedTransaction
 --
 -- Note: this function will overwrite the sender of the transaction!
 signSimple :: SecretKey -> Transaction -> SignedTransaction
-signSimple sk txn = SignedTransaction
-  { stTxn = txn { tSender = fromPublicKey (toPublic sk) }
-  , stSig = SignatureSimple $ sign sk (serialiseTx txn)
-  }
+signSimple sk txn =
+  let txn' = txn { tSender = fromPublicKey (toPublic sk) }
+  in SignedTransaction
+    { stTxn = txn'
+    , stSig = SignatureSimple $ sign sk (serialiseTx txn')
+    }
 
 -- | Verify a simple signature transaction.
 verifySimple :: Signature -> Transaction -> Bool

test/algorand-lib/Test/Data/Algorand/Transaction/Signed.hs
@@ -31,8 +31,8 @@ hprop_sign_verify_simple :: Property
 hprop_sign_verify_simple = property $ do
   Just sk <- skFromBytes <$> forAll genSecretKeyBytes
   tx <- forAll genTransaction
-  let tx' = tx { tSender = fromPublicKey (toPublic sk) }
-  tripping tx' (signSimple sk) verifyTransaction
+  let expectedTx = tx { tSender = fromPublicKey (toPublic sk) }
+  (verifyTransaction $ signSimple sk tx) === Just expectedTx
 
 hprop_signSimple_sets_sender :: Property
 hprop_signSimple_sets_sender = property $ do